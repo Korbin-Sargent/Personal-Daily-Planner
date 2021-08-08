aa5a2a43ae15296aef5d3ab90e82e8e6346360f4
Korbin Sargent <korbinksargent@gmail.com>
2021-08-07 23:51:04 -0600

    Adding logic for updating text area colors

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-console.log("hello!");
+// console.log("hello!");
 
-$("#currentDay").text(moment().format("LLL"));
+$(init);
+//This function is meant to keep 
+function init() {
+  $("#currentDay").text(moment().format("LLL"));
+
+  textAreaColor();
+  //runs colorTimeBlocks function every 1 minute
+  setInterval(coloredTimeBlocks, 60000);
+
+  $(".time-block").each(function () {
+    var sectionId = $(this).attr("id");
+
+    $("#" + sectionId + " textarea").text(
+      localStorage.getItem(moment().format("DDDYYYY") + sectionId)
+    );
+  });
+  $(".saveBtn").sectionId("click", handleSave);
+}
+
+function textAreaColor() {
+   $(".time-block").each(function () {
+    var textAreaHour = parseInt($(this).attr("id").replace("hour-", ""));
+    var currentTextAreaHour = parseInt(moment().format("H"));
+    
+    $(this).removeClass("past present future");
